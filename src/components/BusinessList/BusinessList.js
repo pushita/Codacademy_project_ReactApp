91d203eeb77d2fb70b00f75479428033ae98f1a4
Tania Kudrina <tania.kudrina@gmail.com>
2020-06-06 18:59:00 +0200

    feat/2020-06-06 second part of project,preparation for API set up

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,18 +1,16 @@
 import React from "react";
-import './BusinessList.css';
-import Business from '../business/Business';
+import "./BusinessList.css";
+import Business from "../business/Business";
+
 class BusinessList extends React.Component {
-    render () {
-        return (
-            <div className="BusinessList">
-  <Business />
-  <Business />
-  <Business />
-  <Business />
-  <Business />
-  <Business />
-</div>
-        )
-    }
-};
+  render() {
+    return (
+      <div className="BusinessList">
+        {this.props.businesses.map((business) => {
+          return <Business business={business} />;
+        })}
+      </div>
+    );
+  }
+}
 export default BusinessList;
