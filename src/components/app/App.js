49f1c811b1b6c85bfa15180e1a43ebacf5c7611b
Tania Kudrina <tania.kudrina@gmail.com>
2020-06-26 18:06:57 +0200

    feat: second part of porject,activation of search bar

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -18,10 +18,15 @@ const business = {
 const businesses = [business, business, business, business, business, business];
 
 function App() {
+  function searchYelp(term, location, sortBy) {
+    console.log(
+      `Searching Yelp with ${term}, ${location}, ${sortBy}. The real API will be added on the next steps`
+    );
+  }
   return (
     <div className="App">
       <h1>ravenous</h1>
-      {<SearchBar />}
+      {<SearchBar searchYelp={searchYelp}/>}
       {<BusinessList businesses={businesses} />}
     </div>
   );
