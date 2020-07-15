8fb70b4ca98ab43cc59fbb4e37b42d09531fddb2
Tania Kudrina <tania.kudrina@gmail.com>
2020-07-15 16:26:52 +0200

    Adjust code according to Sergey's comments

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,9 +6,9 @@ class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        {this.props.businesses.map((business) => {
-          return <Business business={business} key={business.id}/>;
-        })}
+        {this.props.businesses.map((business) => (
+          <Business business={business} key={business.id} />
+        ))}
       </div>
     );
   }
